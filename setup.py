213601e0cdc5904895a560a7ea49dafc4d9609ad
Deger Ahmet <adeger@protonmail.com>
2022-03-07 22:46:01 -0500

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	# Here is the module name.
-	name="drf-authentication-with-knox",
+	name="drf-simple-auth",
 
 	# version of the module
 	version="0.0.1",
