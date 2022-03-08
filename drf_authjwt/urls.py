026f8236f6b6f615fdacdfe346f4fd91c8ed04b5
Deger Ahmet <adeger@protonmail.com>
2022-03-07 23:51:21 -0500

    urls updated

--- drf_authjwt/urls.py
+++ drf_authjwt/urls.py
@@ -13,9 +13,9 @@ from .views import (
 from knox import views as knox_views
 
 urlpatterns = [
-    path('auth/login/', LoginAPIView.as_view(), name="login_view"),
-    path('auth/logout/', knox_views.LogoutView.as_view(), name="logout_view"),
-    path('auth/logoutall/', LogoutAllView.as_view(), name="logout_all_view"),
-    path('auth/register/', RegisterAPIView.as_view(), name="register_view"),
-    path('auth/change_password/', ChangePasswordView.as_view(), name='auth_change_password'),
+    path('login/', LoginAPIView.as_view(), name="login_view"),
+    path('logout/', knox_views.LogoutView.as_view(), name="logout_view"),
+    path('logoutall/', LogoutAllView.as_view(), name="logout_all_view"),
+    path('register/', RegisterAPIView.as_view(), name="register_view"),
+    path('change_password/', ChangePasswordView.as_view(), name='auth_change_password'),
 ]
