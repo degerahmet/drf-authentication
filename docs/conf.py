bcf3165184329916fa8562548656792c19ba1af1
Ahmet Değer <adeger34@gmail.com>
2022-03-09 17:38:01 -0500

    update

--- docs/conf.py
+++ docs/conf.py
@@ -1,92 +1,41 @@
-
 from pkg_resources import get_distribution
 
-# -- General configuration ------------------------------------------------
+project = 'drf-auth-simple'
+copyright = '2022, Ahmet Deger'
+author = 'Ahmet Deger'
 
-
-def django_configure():
-    from django.conf import settings
-
-    settings.configure(
-        SECRET_KEY="a random key to use",
-        INSTALLED_APPS=(
-            "django.contrib.admin",
-            "django.contrib.auth",
-            "django.contrib.contenttypes",
-            "django.contrib.sessions",
-            "django.contrib.sites",
-            "django.contrib.staticfiles",
-            "rest_framework",
-            "rest_framework_simplejwt",
-            "rest_framework_simplejwt.token_blacklist",
-        ),
-    )
-
-    try:
-        import django
-
-        django.setup()
-    except AttributeError:
-        pass
-
-
-django_configure()
-
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.doctest",
-    "sphinx.ext.intersphinx",
-]
-html_theme_options = {
-    'analytics_id': 'G-PBD6GQY3WR',  #  Provided by Google in your dashboard
-    'analytics_anonymize_ip': False,
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-}
-
-templates_path = ["_templates"]
-
-source_suffix = ".md"
-
-master_doc = "index"
-
-project = "Django Rest Framework Authentication"
-copyright = "2022, Ahmet Deger"
 
 release = get_distribution("drf-auth-simple").version
 
-# The short X.Y version.
 version = ".".join(release.split(".")[:2])
 
-pygments_style = "sphinx"
 
-html_theme = "sphinx_rtd_theme"
+# -- General configuration ---------------------------------------------------
 
-# html_theme_options = {}
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.autosectionlabel'
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'sphinx-env']
 
 
-htmlhelp_basename = "drf-auth-doc"
+# -- Options for HTML output -------------------------------------------------
 
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
 
-intersphinx_mapping = {
-    "python": ("https://docs.python.org/3.8", None),
-}
-
-# -- Doctest configuration ----------------------------------------
-
-import doctest
-
-doctest_default_flags = (
-    0
-    | doctest.DONT_ACCEPT_TRUE_FOR_1
-    | doctest.ELLIPSIS
-    | doctest.IGNORE_EXCEPTION_DETAIL
-    | doctest.NORMALIZE_WHITESPACE
-)
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
