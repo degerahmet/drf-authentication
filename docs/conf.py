6df0629c020a14401b2e34724c135a629a9dbf06
Ahmet Değer <adeger34@gmail.com>
2022-03-09 16:39:21 -0500

    theme options

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,19 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.intersphinx",
 ]
+html_theme_options = {
+    'analytics_id': 'G-PBD6GQY3WR',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+}
 
 templates_path = ["_templates"]
 
